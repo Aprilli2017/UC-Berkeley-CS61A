877b78b47509aa436423bba7d6797f4cf9658632
Sky-David-Ye <SkyDavidYe@gmail.com>
2015-12-13 14:47:16 +0800

    homework

---
+++ hw/hw03/hw03.py
@@ -0,0 +1,188 @@
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    """
+    "*** YOUR CODE HERE ***"
+    if n <= 3:
+        return n
+    return g(n-1) + 2 * g(n-2) + 3 * g(n-3)
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(i, is_right):
+        if i >= n:
+            return 0;
+        if has_seven(i) or (i % 7 == 0 and i != 0):
+            is_right = not is_right
+        if is_right:
+            return 1 + helper(i+1, is_right)
+        else:
+            return -1 + helper(i+1, is_right)
+    return helper(0, True) 
+
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    if k % 10 == 7:
+        return True
+    elif k < 10:
+        return False
+    else:
+        return has_seven(k // 10)
+
+
+def count_change(amount):
+    """Return the number of ways to make change for amount.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(amount, max_size):
+        if max_size == 1:
+            return 1
+        if amount < max_size:
+            return helper(amount, max_size // 2)
+        elif amount == max_size:
+            return 1 + helper(amount, max_size // 2)
+        else:
+            return helper(amount, max_size // 2) + helper(amount - max_size, max_size)
+        
+    return helper(amount, get_max_cent(amount))
+
+def get_max_cent(amount):
+    i = 0
+    while (pow(2, i) <= amount):
+        i += 1
+    return pow(2, i-1)
+
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        print_move(start, end)
+        return
+    move_stack(n-1, start, 6 - start - end)
+    print_move(start, end)
+    move_stack(n-1, 6 - start - end, end)    
+        
+
+
+
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    """
+    return 'YOUR_EXPRESSION_HERE'
+
